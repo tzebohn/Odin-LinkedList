1dc5f374ecc8feb5244dc9fff01e1b50c650bd3b
tzebohn <tzebohnling@gmail.com>
2025-09-15 23:24:10 -0400

    Created append method for LinkedList
    User is able to add Node to end of LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -9,4 +9,19 @@ class LinkedList {
     constructor () {
         this.head = null
     }
+
+    //Insert the Node at end
+    append (value) {
+        const newNode = new Node(value)
+        
+        if (!this.head) { //First node
+            this.head = newNode
+        } else { //Not first node
+            let current = this.head //Use reference pointer
+            while (current.nextNode) {
+                current = current.nextNode
+            }
+            current.nextNode = newNode
+        }
+    }
 }
